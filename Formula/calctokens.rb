872ffd700226b73c07e9750c00018756a7e220cc
Darchrow <just4signinid@gmail.com>
2026-05-21 21:19:23 +0800

    calctokens 0.8.2

--- Formula/calctokens.rb
+++ Formula/calctokens.rb
@@ -1,16 +1,16 @@
 class Calctokens < Formula
   desc "Token usage report powered by calctokens-core with K/M/B units & RMB conversion"
   homepage "https://github.com/aiyouwolegequ/CalcTokens"
-  version "0.8.0"
+  version "0.8.2"
 
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/aiyouwolegequ/CalcTokens/releases/download/v0.8.0/calctokens-macos-arm64",
-      tag: "v0.8.0"
-    sha256 "f759a24cf427016cfc1a365242bd261814a13841242d4651dbb720f8042bdde7"
+    url "https://github.com/aiyouwolegequ/CalcTokens/releases/download/v0.8.2/calctokens-macos-arm64",
+      tag: "v0.8.2"
+    sha256 "5afd5056b4a9e9df3a0e3d9bd46f88955a649a88882c58c062c8e06d5d57816f"
   else
-    url "https://github.com/aiyouwolegequ/CalcTokens/releases/download/v0.8.0/calctokens",
-      tag: "v0.8.0"
-    sha256 "bd13a51bd385da4bbc87df748a518d25e53c4f5c94367b2002dac5c0d49160e4"
+    url "https://github.com/aiyouwolegequ/CalcTokens/releases/download/v0.8.2/calctokens",
+      tag: "v0.8.2"
+    sha256 "PLACEHOLDER_FOR_OTHER_ARCH"
   end
 
   def install
