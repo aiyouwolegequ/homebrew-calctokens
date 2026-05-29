44d40b40a5c893b17b27e030de6530fa55339ebc
Darchrow <just4signinid@gmail.com>
2026-05-29 13:00:09 +0800

    chore: bump calctokens to v0.9.7

--- Formula/calctokens.rb
+++ Formula/calctokens.rb
@@ -1,16 +1,16 @@
 class Calctokens < Formula
   desc "Token usage report powered by calctokens-core with K/M/B units & RMB conversion"
   homepage "https://github.com/aiyouwolegequ/CalcTokens"
-  version "0.9.6"
+  version "0.9.7"
 
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/aiyouwolegequ/CalcTokens/releases/download/v0.9.6/calctokens-macos-arm64",
-      tag: "v0.9.6"
-    sha256 "31e9df8995c55e5145a5893285827534eca154d2bedd84458799f85e864ba451"
+    url "https://github.com/aiyouwolegequ/CalcTokens/releases/download/v0.9.7/calctokens-macos-arm64",
+      tag: "v0.9.7"
+    sha256 "645181e36245f3e3031e6d292690467e622e7d4a9a76f17233e801a125563722"
   else
-    url "https://github.com/aiyouwolegequ/CalcTokens/releases/download/v0.9.6/calctokens",
-      tag: "v0.9.6"
-    sha256 "2ead26c2fc4a4d085eade27155ff1b9d5aa2158ee27e218df70f495cb4cc8f64"
+    url "https://github.com/aiyouwolegequ/CalcTokens/releases/download/v0.9.7/calctokens",
+      tag: "v0.9.7"
+    sha256 "2cc1d6f7907b772a4abf0795f040fa13de88f9392276bd691d3267ee10a94733"
   end
 
   def install
