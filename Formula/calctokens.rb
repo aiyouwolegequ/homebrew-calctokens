89cae503b2cdbbe455b435b4ee3d8d90f269a8f2
Darchrow <just4signinid@gmail.com>
2026-06-05 21:39:25 +0800

    bump version to v1.0.5

--- Formula/calctokens.rb
+++ Formula/calctokens.rb
@@ -3,13 +3,13 @@ class Calctokens < Formula
   homepage "https://github.com/aiyouwolegequ/CalcTokens"
 
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/aiyouwolegequ/CalcTokens/releases/download/v1.0.4/calctokens-macos-arm64",
-      tag: "v1.0.4"
-    sha256 "fa79ede3c075da1c5af21cbfa1e30151561ca9b1c894955ee6c0705709cb3124"
+    url "https://github.com/aiyouwolegequ/CalcTokens/releases/download/v1.0.5/calctokens-macos-arm64",
+      tag: "v1.0.5"
+    sha256 "9d201780b809f4fa8e26e1f734f6f55281d8fee1a2af209b6db88e73a997c844"
   else
-    url "https://github.com/aiyouwolegequ/CalcTokens/releases/download/v1.0.4/calctokens-linux-x86_64",
-      tag: "v1.0.4"
-    sha256 "a88aa746a2ea0fb4cf658e8950c9604a0ef2a053b1deb06c88abd07abb99bd4d"
+    url "https://github.com/aiyouwolegequ/CalcTokens/releases/download/v1.0.5/calctokens-linux-x86_64",
+      tag: "v1.0.5"
+    sha256 "4e379ae9c06ab9f90e8bbac5705fdf38547ef132371878a405c73982f54b90b4"
   end
 
   def install
